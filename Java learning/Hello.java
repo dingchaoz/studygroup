2e3a05b789d0afd409355974a9e6d4bcc3ec7df3
Dingchao Zhang <dingchaozhang@g.harvard.edu>
2015-05-06 20:05:00 -0400

    first commit

---
+++ Java learning/Hello.java
@@ -0,0 +1,12 @@
+// hello world learning java
+// 2014-12-18
+
+// one java script can only has one public class
+// the class name is Hello, the public class name must be the same with the script name
+public class Hello
+{
+      public statis 
+
+
+
+}
